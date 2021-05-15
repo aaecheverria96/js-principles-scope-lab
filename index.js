958f32c80c2f496d917118a5732e37aa952bc6ea
Your Username <you@example.com>
2021-05-15 17:11:28 -0400

    commit tree fcb7e91bc256b2d6357bc0855366862aa088599d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'; 
+
+function upperCaseCustomerName(){ 
+    customerName = 'BOB'
+} 
+
+function setBestCustomer() { 
+    bestCustomer = 'not bob'; 
+
+}  
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function leastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'bob'
+}
+
+function changeLeastFavoriteCustomer()[ 
+     leastFavoriteCustomer = 'bob'
+]
+
